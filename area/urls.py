f34ffd3294cb5fc136ed41776a9176262c447aa4
Thaynara Sabryna <thaynarasabryna191@hotmail.com>
2024-08-15 21:13:25 -0300

    up total

--- area/urls.py
+++ area/urls.py
@@ -1,6 +1,12 @@
+from django.contrib import admin
 from django.urls import path, include
-from .views import homepage
+from area import views
 
 urlpatterns = [
-    path('', homepage),
+    path('admin/', admin.site.urls),
+    path('', views.login_view, name='login'),
+    path('area/', views.area_view, name='area'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('recurso/', views.recurso_view, name='recurso'),
+    path('configuracoes/', views.configuracoes_view, name='configuracoes'),
 ]
--- area/views.py
+++ area/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-def homepage(request):
-    return render(request, "homepage.html")
+def login_view(request):
+    return render(request, "login.html")
+
+def area_view(request):
+    return render(request, 'area.html')
+
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
+
+def recurso_view(request):
+    return render(request, 'recurso.html')
+
+def configuracoes_view(request):
+    return render(request, 'configuracoes.html')
+
